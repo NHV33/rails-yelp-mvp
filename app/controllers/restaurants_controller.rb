30c9b18791640a6b30072bfd3924de470dda8ae2
Noel Vincent <noelvincent@me.com>
2024-01-11 22:43:12 +0900

    can add new restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,33 @@
+class RestaurantsController < ApplicationController
+
+  # before_action :set_restaurant, only: %i[show edit update destroy]
+  before_action :set_restaurant, only: %i[show]
+
+  def index
+    @restaurants = Restaurant.all.sort_by { |r| r[:id] }.reverse!
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
